4d126425613f16240557d5df1316d00ea0ce3147
appendr <streamd@appendr.com>
2012-10-06 23:28:40 -0400

    clean up

--- streamd/src/test/scala/com/appendr/streamd/sink/SinkTest.scala
+++ streamd/src/test/scala/com/appendr/streamd/sink/SinkTest.scala
@@ -6,7 +6,7 @@ import org.scalatest.junit.JUnitRunner
 import com.appendr.streamd.conf.Configuration
 
 @RunWith(classOf[JUnitRunner])
-class SinkTest extends FunSuite with BeforeAndAfter {
+class FileSinkTest extends FunSuite with BeforeAndAfter {
     private val sink = new FileSink
 
     before {
@@ -20,5 +20,24 @@ class SinkTest extends FunSuite with BeforeAndAfter {
 
     test("write to sink") {
         sink.out("Hello World!")
+    }
+}
+
+//@RunWith(classOf[JUnitRunner])
+class WebSocketSinkTest extends FunSuite with BeforeAndAfter {
+    private val sink = new WebSocketSink
+
+    before {
+        val conf = Configuration.fromResource("test.conf")
+        sink.open(conf.getSection("streamd.plugin.sink"))
+    }
+
+    after {
+        sink.close()
+    }
+
+    test("write to sink") {
+        sink.out("Hello World!")
+        Thread.sleep(1000)
     }
 }
